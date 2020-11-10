006cf0dad0272eec2296cba754d7541118ccafce
Lang-Cheng <lang_cl@outlook.com>
2020-11-10 11:25:46 +0800

    Initial Commit

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,26 @@
+# Disable default keys
+Remove-PSReadlineKeyHandler 'Ctrl+r'
+
+# Import Modules
+Import-Module posh-git
+Import-Module oh-my-posh
+Import-Module PSFzf
+Import-Module PoShFuck
+
+# Set Theme
+Set-Theme Paradox
+
+# Set MenuComplete
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+# Alias Command
+Set-Alias -Name n -Value nvim
+
+# Alias Software
+Set-Alias -Name vscode -Value "C:\Users\langc\AppData\Local\Programs\Microsoft VS Code\Code.exe"
+
+# Customized Functions
+function lsc {
+   Param ([int]$c = 5)
+   ls | Format-Wide -Column $c -Property Name
+}
