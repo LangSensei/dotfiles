eeb0f6178942cf3f2e7afa0818377c883a10dabf
Lang Cheng <lang_cl@outlook.com>
2021-06-17 15:33:43 +0800

    Update Microsoft.PowerShell_profile.ps1

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -3,12 +3,6 @@ if ($env:TERM_PROGRAM -eq "vscode") {
    Set-PSReadLineKeyHandler -Chord 'Ctrl+w' -Function BackwardKillWord
 }
 
-# PSFzf
-Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
-
-# Auto Complete
-Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
-
 # Import Modules
 Import-Module posh-git
 Import-Module oh-my-posh
@@ -17,11 +11,21 @@ Import-Module PSFzf
 # Set Theme
 Set-PoshPrompt -Theme iterm2
 
+# Auto Complete
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+# PSFzf
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
+
 # Alias Command
 Set-Alias -Name n -Value nvim
 
 # Customized Functions
-function lsc {
-   Param ([int]$c = 5)
+function lsc
+{
+   Param (
+      [int]$c = 5
+   )
+
    Get-ChildItem | Format-Wide -Column $c -Property Name
 }
