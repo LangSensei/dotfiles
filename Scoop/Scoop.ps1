8b1ac32ec29648a608190afc24bd5beafabb3422
Lang Cheng <lang_cl@outlook.com>
2022-08-27 01:12:25 +0800

    Update Scoop.ps1

--- Scoop/Scoop.ps1
+++ Scoop/Scoop.ps1
@@ -1,5 +1,5 @@
 # System Variable
-$env:SCOOP='C:\Applications'
+$env:SCOOP='C:\ScoopApps'
 [Environment]::SetEnvironmentVariable('SCOOP', $env:SCOOP, 'User')
-$env:SCOOP_GLOBAL='D:\Applications'
+$env:SCOOP_GLOBAL='D:\ScoopApps'
 [Environment]::SetEnvironmentVariable('SCOOP_GLOBAL', $env:SCOOP_GLOBAL, 'Machine')
